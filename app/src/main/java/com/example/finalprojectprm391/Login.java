2d3770ca7feb4d8e3676281a703acafb278156f6
tientien99 <domanhtien28051999@gmail.com>
2020-10-30 00:50:31 +0700

    buil login

--- app/src/main/java/com/example/finalprojectprm391/Login.java
+++ app/src/main/java/com/example/finalprojectprm391/Login.java
@@ -5,13 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
